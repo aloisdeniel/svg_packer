a07ca6f744bda8f26fffa39e3d5dee44db91a6b0
Aloïs DENIEL <alois.deniel@gmail.com>
2025-04-25 23:36:56 +0200

    Fixes

--- lib/svg_packer.dart
+++ lib/svg_packer.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as path;
-import 'package:recase/recase.dart';
 import 'package:svg_packer/src/generators/dart.dart';
 import 'package:svg_packer/src/packer.dart';
 
